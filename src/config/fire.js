c42b47288eb585168650c085390c42b8b9765bd1
Pinninti-Venkatesh <41535509+Pinninti-Venkatesh@users.noreply.github.com>
2021-04-21 21:22:56 +0530

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCRogiWqX14J-GQub6YN25ozakMiRc4tgk",
-    authDomain: "sharemusic-7f76e.firebaseapp.com",
-    databaseURL: "https://sharemusic-7f76e.firebaseio.com",
-    projectId: "sharemusic-7f76e",
-    storageBucket: "sharemusic-7f76e.appspot.com",
-    messagingSenderId: "56498364983",
-    appId: "1:56498364983:web:e27ef5f3df95d024fa55b2",
-    measurementId: "G-TJ43VPQWP3"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   const fire=firebase.initializeApp(firebaseConfig);
   
